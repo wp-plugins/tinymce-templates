29ff5cfac093bfba0e0b5018f011a577e4cf5129
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-20 03:24:59 +0000

    tagging 0.31
    git-svn-id: https://plugins.svn.wordpress.org/tinymce-templates/tags/0.31@324441 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/TinyMCETemplate.class.php
+++ includes/TinyMCETemplate.class.php
@@ -4,38 +4,33 @@ new TinyMCETemplate();
 
 class TinyMCETemplate{
 
-    private $mce_css = null;
+    private $list_url = null;
 
     function __construct()
     {
-        $plugin = TINYMCE_TEMPLATES_PLUGIN_URL.'/mce_plugins/plugins/template/editor_plugin.js';
-        $path = dirname(__FILE__).'/../mce_plugins/plugins';
-        $lang = $path.'/template/langs/langs.php';
-        $inits = array();
-        $url = get_bloginfo('url');
-        $inits['template_external_list_url'] = $url.'/mce_templates.js';
-        $this->mce_css = TINYMCE_TEMPLATES_PLUGIN_URL.'/editor.css';
-        $inits['content_css'] = $this->mce_css;
-        new mcePlugins(
-            'template',
-            $plugin,
-            $lang,
-            array(&$this, 'add_button'),
-            $inits
+        new AddRewriteRules(
+            'mce_templates.js$',
+            'mce_templates',
+            array(&$this, 'get_templates')
         );
-
-        add_filter('mce_css', array(&$this, 'addCSS'));
-        add_filter('query_vars', array(&$this, 'query_vars'));
-        add_filter('rewrite_rules_array', array(&$this, 'rewrite_rules_array'));
-        add_action('init', array(&$this, 'init'));
-        add_action('wp', array(&$this, 'wp'));
+        add_filter('mce_css', array(&$this, 'addStyle'));
+        add_action('init', array(&$this, 'loadTinyMCEPlugin'));
     }
 
-    public function addCSS($css)
+//
+// add css tinyMCE content
+//
+    public function addStyle($css)
     {
-        return $css.','.$this->mce_css;
+        $files = preg_split("/,/", $css);
+        $files[] = TINYMCE_TEMPLATES_PLUGIN_URL.'/editor.css';
+        $files = array_map('trim', $files);
+        return join(",", $files);
     }
 
+//
+// add button to tinyMCE editor
+//
     public function add_button($buttons = array())
     {
         array_unshift($buttons, '|');
@@ -43,27 +38,36 @@ class TinyMCETemplate{
         return $buttons;
     }
 
-    public function init(){
+//
+// load tinyMCE plugin
+//
+    public function loadTinyMCEPlugin(){
         global $wp_rewrite;
-        $rules = $wp_rewrite->wp_rewrite_rules();
-        if (!isset($rules['mce_templates.js$'])) {
-            $wp_rewrite->flush_rules();
+        $plugin = TINYMCE_TEMPLATES_PLUGIN_URL.'/mce_plugins/plugins/template/editor_plugin.js';
+        $path = dirname(__FILE__).'/../mce_plugins/plugins';
+        $lang = $path.'/template/langs/langs.php';
+        $inits = array();
+        $url = get_bloginfo('url');
+        if ($wp_rewrite->using_permalinks()) {
+            $this->list_url = $url.'/mce_templates.js';
+        } else {
+            $this->list_url = $url.'/?mce_templates=1';
         }
+        $inits['template_external_list_url'] = $this->list_url;
+        new mcePlugins(
+            'template',
+            $plugin,
+            $lang,
+            array(&$this, 'add_button'),
+            $inits
+        );
     }
 
-    public function rewrite_rules_array($rules){
+//
+// return display templates as JSON
+//
+    public function get_templates(){
         global $wp_rewrite;
-        $new_rules['mce_templates.js$'] = $wp_rewrite->index . '?mce_templates=1';
-        $rules = array_merge($new_rules, $rules);
-        return $rules;
-    }
-
-    public function query_vars($vars) {
-        $vars[] = 'mce_templates';
-        return $vars;
-    }
-    
-    public function wp(){
         if (get_query_var('mce_templates')) {
             global $wpdb;
             global $user_login;
@@ -94,11 +98,16 @@ class TinyMCETemplate{
             header( 'Content-Type: application/x-javascript; charset=UTF-8' );
             echo 'var tinyMCETemplateList = [';
             $arr = array();
+            if ($wp_rewrite->using_permalinks()) {
+                $list_url = $this->list_url.'?';
+            } else {
+                $list_url = $this->list_url.'&';
+            }
             foreach ($row as $tpl) {
                 $ID = esc_html($tpl->ID);
                 $name = $tpl->name;
                 $desc = esc_html($tpl->desc);
-                $arr[] = "[\"{$name}\", \"/mce_templates.js?id={$ID}\", \"{$desc}\"]";
+                $arr[] = "[\"{$name}\", \"{$list_url}id={$ID}\", \"{$desc}\"]";
             }
             echo join(',', $arr);
             echo ']';
